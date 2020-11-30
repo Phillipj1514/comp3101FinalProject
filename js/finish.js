bd3289e1af4913b9a1896d3f153b7a6a62f793ea
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2020-11-30 13:48:55 -0500

    Started adding images

--- js/finish.js
+++ js/finish.js
@@ -12,11 +12,9 @@ function animate(){
     requestAnimationFrame(animate)
     c.clearRect(0, 0, innerWidth, innerHeight)
     //dma
-    c.fillStyle = "rgba(0, 200, 0, .5)"
-    c.fillRect(550, 200, 150, 300)
+    
     //cpu
-    c.fillStyle = "rgba(200, 0, 0, .5)"
-    c.fillRect(150, 150, 175, 175)
+    
 
     //memory
 
@@ -25,13 +23,13 @@ function animate(){
     c.lineWidth = 7;
     c.moveTo(325,240)
     c.lineTo(550,240)
-    c.strokeStyle = "grey"
+    c.strokeStyle = "#ce9417"
     c.stroke();
 
     //message
     c.beginPath()
     c.arc(x, 240, 4, 0, Math.PI * 2, false);
-    c.strokeStyle = "blue"
+    c.strokeStyle = "#2e92e4"
     c.stroke()
     if(x > 325){
         x -= 1;
