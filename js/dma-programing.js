89234d15adae83939a341fdf3411c31f1d2c648a
Roneil Barrett <roneilbarrett@gmail.com>
2020-12-02 00:39:02 -0500

    Added DMA Programming to standard frame for easier viewing

--- js/dma-programing.js
+++ js/dma-programing.js
@@ -1,25 +1,28 @@
+// CANVAS INITIALIZATION //
 const canvas = document.getElementById('canvas');
-canvas.width = 1000;
-canvas.height = 600;
+canvas.width = 1225;
+canvas.height = 500;
 const ctx = canvas.getContext('2d');
-const sidePanel = document.getElementById('side-panel');
+
+// GET CONTROLS & TEXT PANEL FROM DOM
+const sidePanel = document.getElementsByClassName("text-region")[0];
 const playButton = document.getElementById("play-button");
 const stopButton = document.getElementById("stop-button");
 const backButton = document.getElementById("back-button");
 const nextButton = document.getElementById("next-button");
 
+// GET IMAGES FROM DOM
 const cpuImg = document.getElementById("cpu");
 const dmaImg = document.getElementById("dma");
 const memoryImg = document.getElementById("memory");
 const controllerImg = document.getElementById("controller");
 const deviceImg = document.getElementById("device");
 
+// ANNOTATIONS
 let deviceReady = "The Device controller’s Status Register indicates that bytes are ready to be transferred.";
 let dmaRequest = "A DMA Request a.k.a (DRQ) is then issued to the DMA Controller";
-
 let holdRequest = "The DMA Controller now issues a HOLD Request (HRQ) to the CPU";
 let fillBuffer = "DMA starts filling its internal buffer while awaiting the HOLD ACKNOWLEDGEMENT";
-
 let holdAckReceived = "The DMA receives the HOLD ACKNOWLEDGEMENT from the cpu";
 let cpuRequestReceived = "The CPU receives the HOLD REQUEST from the DMA";
 let cpuAckSent = "CPU sends the HOLD ACKNOWLEDGEMENT to the DMA";
@@ -30,29 +33,56 @@ let interrupt = "An interrupt to the CPU is now in order.";
 // The CPU component
 const cpu = {
     name: "CPU",
-    x: 500,
-    y: 25,
-    width: 150,
-    height: 80,
+    x: 525,
+    y: 0,
+    width: 100,
+    height: 70,
     inside: true,
     color: "#5cb85c"
 }
 
+// CPU TO CACHE CONNECTION
+const cpu_cac = {
+    x: cpu.x+(cpu.width/2)-5,
+    y: cpu.y+cpu.height-8,
+    w: 10,
+    h: 50
+}
+
 const cache = {
     name: "Cache",
-    x: cpu.x + 25,
-    y: cpu.y + 130,
-    width: 100,
+    x: cpu.x + cpu.width/4,
+    y: cpu.y + cpu.height + cpu_cac.h-8,
+    width: 50,
     height: cpu.height/2,
     inside: true,
     color: "#d9534f"
 }
 
+// CACHE TO MEMORY-BUS CONNECTION
+const cac_mem = {
+    x: cache.x+(cache.width/2)-5,
+    y: cache.y+cache.height,
+    w: 10,
+    h: 75
+}
+
+// CPU TO MEMORY BUS
+const cpuToMemoryBus = {
+    name: "CPU - Memory Bus",
+    x: cpu.x-cpu.width+20,
+    y: cac_mem.y+cac_mem.h,
+    width: 250,
+    height: 20,
+    inside: true,
+    color: "#1bafdf"
+}
+
 // The main memory component
 const memory = {
     name: "Memory",
-    x: 700,
-    y: 250,
+    x: cpuToMemoryBus.x+cpuToMemoryBus.width-25,
+    y: cpuToMemoryBus.y-48,
     width: cpu.width,
     height: cpu.height,
     inside: true,
@@ -62,72 +92,48 @@ const memory = {
 // The DMA Controller component
 const dma = {
     name: "DMA Controller",
-    x: 300,
-    y: 215,
-    width: 150,
-    height: 150,
+    x: cpuToMemoryBus.x-60,
+    y: cpuToMemoryBus.y-20,
+    width: 60,
+    height: 60,
     color: "#5cb85c"
 }
-// The DMA Address Register
-const addressRegister = {
-    name: "Address",
-    x: dma.x + (dma.width/2) - 30,
-    y: dma.y + 15,
-    width: 60,
-    height: 20,
-    inside: true,
-    color: "#d6c576"
-}
-// The DMA Count Register
-const countRegister = {
-    name: "Count",
-    x: addressRegister.x,
-    y: addressRegister.y+50,
-    width: addressRegister.width,
-    height: addressRegister.height,
-    inside: true,
-    color: "#d6c576"
-}
-// The DMA Control Register
-const controlRegister = {
-    name: "Control",
-    x: countRegister.x,
-    y: countRegister.y+50,
-    width: countRegister.width,
-    height: countRegister.height,
-    inside: true,
-    color: "#d6c576"
+
+// PCI BUS TO DMA CONTROLLER CONNECTION
+const pci_dma = {
+    x: dma.x+(dma.width/2)-5,
+    y: dma.y+dma.height,
+    w: 10,
+    h: 55
 }
 
-
-const cpuToMemoryBus = {
-    name: "CPU - Memory Bus",
-    x: dma.x+dma.width,
-    y: memory.y+memory.height/4,
-    width: 250,
-    height: 40,
-    inside: true,
-    color: "#1bafdf"
-}
-
+// THE PCI BUS
 const pciBus = {
     name: "PCI Bus",
     x: 0,
-    y: 420,
+    y: pci_dma.y+pci_dma.h,
     width: canvas.width,
-    height: 40,
+    height: 20,
     inside: true,
     color: "#1bafdf"
+}
+
+// PCI BUS TO DEVICE CONTROLLER CONNECTION
+const pci_con = {
+    x: 125,
+    y: pciBus.y+pciBus.height,
+    w: 10,
+    h: 50
 }
 
 // The Device Controller Component
 // Represents a generic device controller
 const controller = {
     name: "Device Controller",
-    x: 70,
-    y: 510,
-    width: 120,
-    height: 70,
+    x: pci_con.x-35,
+    y: pci_con.y+pci_con.h+15,
+    width: 80,
+    height: 10,
     inside: true,
     color: "#5cb85c"
 }
@@ -152,37 +158,6 @@ const con_dev = {
     h: 10
 }
 
-// PCI BUS TO DEVICE CONTROLLER CONNECTION
-const pci_con = {
-    x: controller.x+(controller.width/2)-5,
-    y: pciBus.y+pciBus.height,
-    w: 10,
-    h: 50
-}
-
-// PCI BUS TO DMA CONTROLLER CONNECTION
-const pci_dma = {
-    x: dma.x+(dma.width/2)-5,
-    y: dma.y+dma.height,
-    w: 10,
-    h: 55
-}
-
-// CPU TO CACHE CONNECTION
-const cpu_cac = {
-    x: cpu.x+(cpu.width/2)-5,
-    y: cpu.y+cpu.height,
-    w: 10,
-    h: 50
-}
-
-// CACHE TO MEMORY-BUS CONNECTION
-const cac_mem = {
-    x: cache.x+(cache.width/2)-5,
-    y: cache.y+cache.height,
-    w: 10,
-    h: 75
-}
 
 // The starting transfer object
 const instructionObj = {
@@ -236,7 +211,7 @@ const dataObj3 = {
     stops: 0
 }
 
-// Function to draw a component
+// FUNCTION TO DRAW A RECTANGULAR COMPONENT //
 drawRect = (obj) => {
     ctx.fillStyle = obj.color;
     ctx.fillRect(obj.x,obj.y,obj.width,obj.height);
@@ -251,6 +226,7 @@ drawRect = (obj) => {
     }
 }
 
+// FUNCTION TO DRAW A CIRCLE
 drawCircle = (obj) => {
     ctx.fillStyle = obj.color;
     ctx.beginPath();
@@ -261,17 +237,18 @@ drawCircle = (obj) => {
     ctx.fill();
 }
 
+// FUNCTION TO DRAW A SIMPLE CONNECTION TO BUS OR OTHER OBJECTS //
 drawConnection = (conn) => {
     ctx.fillStyle = "gray";
     ctx.fillRect(conn.x,conn.y,conn.w,conn.h);
 }
 
-
-let stop = 0;
+// FUNCTION FOR CONTROLLING OBJECT VELOCITY AND DIRECTION //
 function transfer(obj,dist,dir) {
     
     ctx.beginPath();
-    
+
+    // STATEMENTS THAT MANIPULATE OBJEECT COLOR & SHAPE //
     if (obj.stops < 3 && obj.name == "Inst. obj") {
         ctx.arc(obj.x, obj.y, 8, 0, Math.PI * 2);
         ctx.fillStyle = "red";
@@ -289,10 +266,10 @@ function transfer(obj,dist,dir) {
         ctx.fillStyle = "black";
         ctx.fill();
     }
-    console.log(obj.name, obj.stops);
+
+    // SWITCH STATEMENT TO CONTROL OBJECT DIRECTION //
     switch (dir) {
         case "r":
-            console.log(obj.name,"going right");
             if (obj.state == false) {        
                 obj.newx = obj.x + dist;
                 obj.state = true;
@@ -304,11 +281,9 @@ function transfer(obj,dist,dir) {
             if(obj.x >= obj.newx){
                 obj.state = false;
                 obj.stops += 1;
-                console.log(obj.name,"reached",obj.stops);
             }
             break;
         case "l":
-            console.log(obj.name,"going left");
             if (obj.state == false) {
                 obj.newx = obj.x - dist;
                 obj.state = true;
@@ -320,11 +295,9 @@ function transfer(obj,dist,dir) {
             if(obj.x <= obj.newx){
                 obj.state = false;
                 obj.stops += 1;
-                console.log(obj.name,"reached",obj.stops);
             }
             break;
         case "u":
-            console.log(obj.name,"going up");
             if (obj.state == false) {
                 obj.newy = obj.y - dist;
                 obj.state = true;
@@ -336,11 +309,9 @@ function transfer(obj,dist,dir) {
             if(obj.y <= obj.newy){
                 obj.state = false;
                 obj.stops += 1;
-                console.log(obj.name,"reached", obj.stops);
             }
             break;
         case "d":
-            console.log(obj.name,"going down");
             if (obj.state == false) {
                 obj.newy = obj.y + dist;
                 obj.state = true;
@@ -352,7 +323,6 @@ function transfer(obj,dist,dir) {
             if(obj.y >= obj.newy){
                 obj.state = false;
                 obj.stops += 1;
-                console.log(obj.name,"reached", obj.stops);
             }
             break;
         default:
@@ -362,28 +332,21 @@ function transfer(obj,dist,dir) {
 
 // DRAW COMPONENTS ON THE CANVAS
 drawComponents = () => {
-    ctx.drawImage(cpuImg,500, 17,150,100);
-    //drawRect(cpu);
+    // DRAW MAIN COMPONENTS
+    ctx.drawImage(cpuImg,cpu.x, cpu.y,cpu.width,cpu.height);
     drawRect(cache);
-    ctx.drawImage(memoryImg,680, 230, 150,80);
-    //drawRect(memory);
+    drawConnection(cpu_cac); // CPU to cache connection
+    drawConnection(cac_mem); // Cache to Memory Bus
+    drawRect(cpuToMemoryBus); 
     ctx.drawImage(dmaImg,dma.x,dma.y, dma.width,dma.height);
-    // drawRect(dma);
-    // drawRect(addressRegister);
-    // drawRect(countRegister);
-    // drawRect(controlRegister);
-    drawRect(cpuToMemoryBus);
     drawRect(pciBus);
     ctx.drawImage(controllerImg,controller.x, controller.y-28, controller.width+2,controller.height+50);
-    //drawRect(controller);
     ctx.drawImage(deviceImg,device.x-25, device.y-23, device.width,device.height);
-    //drawCircle(device);
     drawConnection(con_dev);    
     drawConnection(pci_con); // Device Controller to PCI bus connection
     drawConnection(pci_dma); // DMA Controller to PCI Bus
-    drawConnection(cpu_cac); // CPU to cache connection
-    drawConnection(cac_mem); // Cache to Memory Bus
-    
+
+    // SIDE PANEL ANNOTATION //
     if (instructionObj.stops < 3) {
         annotate(deviceReady, sidePanel);   // Annotate the side panel
         annotate(dmaRequest, sidePanel);  // Annotate the side panel
@@ -418,44 +381,43 @@ drawComponents = () => {
         annotate(bytesSent,sidePanel);
         annotate(interrupt,sidePanel);
     }
-
-
 }
 
+// FUNCTION TO CONTROL OBJECT PATHS (ROUTE) //
 animate = () => {
     if (instructionObj.stops == 0) {
-        transfer(instructionObj, pci_con.h+40,"u");
+        transfer(instructionObj, pci_con.h+30,"u");
     } else if (instructionObj.stops == 1){
         transfer(instructionObj, pci_dma.x-pci_con.x,"r"); 
     } else if (instructionObj.stops == 2){
-        transfer(instructionObj, pci_dma.h+95,"u");
+        transfer(instructionObj, pci_dma.h+40,"u");
     } else if (instructionObj.stops == 3){
-        transfer(instructionObj, cpuToMemoryBus.width-50,"r"); // from dma to cpu-mem bus
+        transfer(instructionObj, cpuToMemoryBus.width-90,"r"); // from dma to cpu-mem bus
         if (dataObj.stops == 0) {
-            // controller begins data transfer to dma
-            transfer(dataObj, pci_con.h+40,"u"); 
+            // Controller begins data transfer to dma
+            transfer(dataObj, pci_con.h+30,"u"); 
         }  
     } else if (instructionObj.stops == 4){
-        transfer(instructionObj, cac_mem.h+cpu_cac.h+100,"u");
+        transfer(instructionObj, cac_mem.h+cpu_cac.h+70,"u");
     } else if (instructionObj.stops == 5){
-        transfer(instructionObj, pci_con.h+cac_mem.h+103,"d"); // sending hold acknowledgement
+        transfer(instructionObj, pci_con.h+cac_mem.h+70,"d"); // sending hold acknowledgement
     } else if (instructionObj.stops == 6){
-       transfer(instructionObj, (cpuToMemoryBus.width/2)+30,"l"); // sending hold acknowledgement
+       transfer(instructionObj, (cpuToMemoryBus.width/2)+25,"l"); // sending hold acknowledgement
     } else if (instructionObj.stops == 7){
         // DMA RECEIVES HOLD ACKNOWLEDGEMENT
         // IF DATA OBJECT ARRIVES AT DMA - BEGIN TRANSFER TO MEMORYY
-       if (dataObj.stops == 3) {
-        transfer(dataObj, cpuToMemoryBus.width+80,"r"); // send dataobj from dma to memory
+        if (dataObj.stops == 3) {
+            transfer(dataObj, cpuToMemoryBus.width+30,"r"); // send dataobj from dma to memory
             console.log("Block 1 successfully transferred to memory");
         }
         // IF DATA OBJECT 2 ARRIVES AT DMA and dataobj has been sent to memory
         if (dataObj2.stops == 3 && dataObj.stops == 4) {
-            transfer(dataObj2, cpuToMemoryBus.width+80,"r"); // send dataobj2 from dma to memory     
+            transfer(dataObj2, cpuToMemoryBus.width+30,"r"); // send dataobj2 from dma to memory     
             console.log("Block 2 successfully transferred to memory");
         }
         // IF DATA OBJECT 3 ARRIVES AT DMA and dataobj2 has been sent to memory
         if (dataObj3.stops == 3 && dataObj2.stops == 4) {
-            transfer(dataObj3, cpuToMemoryBus.width+80,"r"); // send dataobj2 from dma to memory     
+            transfer(dataObj3, cpuToMemoryBus.width+30,"r"); // send dataobj2 from dma to memory     
             console.log("Block 3 successfully transferred to memory");
         }
     }
@@ -465,10 +427,10 @@ animate = () => {
         transfer(dataObj, pci_dma.x-pci_con.x,"r"); 
         if (dataObj2.stops == 0) {
             // controller starts transfer of a second block of data to dma
-            transfer(dataObj2, pci_con.h+40,"u"); 
+            transfer(dataObj2, pci_con.h+30,"u"); 
         } 
     } else if (dataObj.stops == 2){
-        transfer(dataObj, pci_dma.h+95,"u");
+        transfer(dataObj, pci_dma.h+40,"u");
     } else if (dataObj.stops == 3){
         console.log("Block 1 successfully sent to dma");
     }
@@ -478,10 +440,10 @@ animate = () => {
         transfer(dataObj2, pci_dma.x-pci_con.x,"r");
         if (dataObj3.stops == 0) {
             // controller starts transfer of a third block of data to dma
-            transfer(dataObj3, pci_con.h+40,"u"); 
+            transfer(dataObj3, pci_con.h+30,"u"); 
         }  
     } else if (dataObj2.stops == 2){
-        transfer(dataObj2, pci_dma.h+95,"u");
+        transfer(dataObj2, pci_dma.h+40,"u");
     } else if (dataObj2.stops == 3){
         console.log("Block 2 successfully sent to dma");
     }
@@ -491,12 +453,13 @@ animate = () => {
         transfer(dataObj3, pci_dma.x-pci_con.x,"r");
         // DATA TRANSFER 4 WOULD GO HERE 
     } else if (dataObj3.stops == 2){
-        transfer(dataObj3, pci_dma.h+95,"u");
+        transfer(dataObj3, pci_dma.h+40,"u");
     } else if (dataObj3.stops == 3){
         console.log("Block 3 successfully sent to dma");
     }
 }
 
+// FUNCTION FOR ANNOTATING SIDE PANEL
 annotate = (text, thisElement) => {
     let ptag = document.createElement("P");           // Create a <p> node
     let string = document.createTextNode(text);      // Create a text node
@@ -504,6 +467,7 @@ annotate = (text, thisElement) => {
     thisElement.appendChild(ptag);   
 }
 
+// FUNCTION FOR REFRESHING SIDE PANEL
 clearPanel = (thisElement) => {
     while (thisElement.hasChildNodes()) {  
         thisElement.removeChild(thisElement.firstChild);
@@ -511,6 +475,7 @@ clearPanel = (thisElement) => {
 }
 
 let mainStarted = false;
+// THE DRIVER FUNCTION
 main = () => {
     if (mainStarted == false) {
         ctx.clearRect(0,0, canvas.width, canvas.height);
@@ -529,7 +494,8 @@ main = () => {
     });
 }
 
-drawComponents();
+drawComponents(); // Draw initial components
 
+// The play button starts the main function
 playButton.addEventListener("click", main);
 
